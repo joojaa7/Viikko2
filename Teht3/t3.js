fc3520f6a438816c62295d439721c087b2e9de00
joojaa7 <Mikael.Asikainen@gmail.com>
2024-03-25 13:35:02 +0200

    teht

--- Teht3/t3.js
+++ Teht3/t3.js
@@ -3,16 +3,16 @@ const side2 = +prompt("Sivu2");
 const side3 = +prompt("Sivu3");
 let answer;
 
-
-if (side1 === side2 && side2 === side3 && side1 === side3){
+if (side1 === side2 && side2 === side3 && side1 === side3) {
   answer = "Equilateral";
-}
-else if (side1 === side2 && side2 != side3 || side1 != side2 && side2 === side3 ||
-         side1 === side3 && side2 != side3){
+} else if (
+  (side1 === side2 && side2 != side3) ||
+  (side1 != side2 && side2 === side3) ||
+  (side1 === side3 && side2 != side3)
+) {
   answer = "Isoleces";
-}
-else {
+} else {
   answer = "None are equal.";
 }
 
-document.querySelector('#id').innerHTML = answer;
+document.querySelector("#id").innerHTML = answer;
