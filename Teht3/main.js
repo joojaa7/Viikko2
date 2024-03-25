390b7a59ae9372259a377c1e3c5eed464bed5c6b
joojaa7 <Mikael.Asikainen@gmail.com>
2024-03-25 13:15:50 +0200

    tehtäviä

--- Teht3/main.js
+++ Teht3/main.js
@@ -3,14 +3,15 @@ const side2 = +prompt("Sivu2");
 const side3 = +prompt("Sivu3");
 let answer;
 
+
 if (side1 === side2 && side2 === side3 && side1 === side3){
   answer = "Equilateral";
 }
 else if (side1 === side2 && side2 != side3 || side1 != side2 && side2 === side3 || side1 === side3 && side2 != side3){
-  answer = "Isoleces"
+  answer = "Isoleces";
 }
 else {
-  answer = "None are equal."
+  answer = "None are equal.";
 }
 
 document.querySelector('#id').innerHTML = answer;
