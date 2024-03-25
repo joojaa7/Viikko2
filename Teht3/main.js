feb86b6932225a5e9821c648b1b55049b70e5142
joojaa7 <Mikael.Asikainen@gmail.com>
2024-03-25 12:52:17 +0200

    tehtäviä

---
+++ Teht3/main.js
@@ -0,0 +1,16 @@
+const side1 = +prompt("Sivu1");
+const side2 = +prompt("Sivu2");
+const side3 = +prompt("Sivu3");
+let answer;
+
+if (side1 === side2 && side2 === side3 && side1 === side3){
+  answer = "Equilateral";
+}
+else if (side1 === side2 && side2 != side3 || side1 != side2 && side2 === side3 || side1 === side3 && side2 != side3){
+  answer = "Isoleces"
+}
+else {
+  answer = "None are equal."
+}
+
+document.querySelector('#id').innerHTML = answer;
